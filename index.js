8fbd53ce380ae8ce928a5a99bab9356abc82d738
jdeluna06 <jdeluna0672@gmail.com>
2024-08-11 20:56:46 -0700

    added question on functions to generateMarkdown

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Enter the project name?",
+    name: "title",
+  },
+
+  {
+    type: "input",
+    message:
+      "Description: Provide a short description explaining the what, why, and how of your project.",
+    name: "description",
+  },
+
+  {
+    type: "input",
+    message: "Instalation: Provide installation instructions for your project.",
+    name: "installation",
+  },
+
+  {
+    type: "input",
+    message: "Usage: Provide instructions and examples for use",
+    name: "usage",
+  },
+
+  {
+    type: "input",
+    message:
+      "Credits: List your collaborators, if any, with links to their GitHub profiles.",
+    name: "credits",
+  },
+
+  {
+    type: "list",
+    message: "Select a license.",
+    choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3-Clause"],
+  },
+
+  {
+    type: "input",
+    message: "Features: If your project has a lot of features, list them here.",
+    name: "features",
+  },
+
+  {
+    type: "input",
+    message: "Enter test instructions.",
+    name: "test",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+	if (err) {
+	  console.error(err);
+	} else {
+	  console.log('README file created successfully!');
+	}
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+    if (!license) {
+      return "";
+    }
+
+    switch (license) {
+      case "MIT":
+        return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      case "GPLv3":
+        return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+      case "Apache 2.0":
+        return "![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-green.svg)";
+      case "BSD 3-Clause":
+        return "![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)";
+      default:
+        return "";
+    }
+    
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+     if (!license) {
+       return "";
+     }
+
+     switch (license) {
+       case "MIT":
+         return "https://opensource.org/licenses/MIT";
+       case "GPLv3":
+         return "https://www.gnu.org/licenses/gpl-3.0";
+       case "Apache 2.0":
+         return "https://www.apache.org/licenses/LICENSE-2.0";
+       case "BSD 3-Clause":
+         return "https://opensource.org/licenses/BSD-3-Clause";
+       default:
+         return "";
+     }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+      if (!license) {
+        return "";
+      }
+
+      const licenseLink = renderLicenseLink(license);
+      return `## License
+
+This project is licensed under the ${license} License. For more information, please visit [this link](${licenseLink}).
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+   const licenseBadge = renderLicenseBadge(data.license);
+   const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+  ${licenseBadge}
+
+${licenseSection}
 `;
 }
 
